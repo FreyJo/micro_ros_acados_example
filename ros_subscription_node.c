1a1e187251912eda4bc5ea74faf488de2415d860
Jonathan Frey <jonathanpaulfrey@gmail.com>
2022-11-17 16:59:11 +0100

    move acados simulation into ros example file

--- ros_subscription_node.c
+++ ros_subscription_node.c
@@ -18,30 +18,96 @@
 #include <rclc/executor.h>
 #include <rclc/rclc.h>
 
+
+// acados
+#include "acados/utils/print.h"
+#include "acados/utils/math.h"
+#include "acados_c/sim_interface.h"
+#include "acados_sim_solver_pendulum_ode.h"
+
+#define NX     PENDULUM_ODE_NX
+#define NZ     PENDULUM_ODE_NZ
+#define NU     PENDULUM_ODE_NU
+#define NP     PENDULUM_ODE_NP
+
+
+
 // these data structures for the publisher and subscriber are global, so that
 // they can be configured in main() and can be used in the corresponding callback.
 rcl_publisher_t my_pub;
 std_msgs__msg__Float64 pub_msg;
 std_msgs__msg__Float64 sub_msg;
 
+double x_current[NX];
+
 /***************************** CALLBACKS ***********************************/
 
 void my_subscriber_callback(const void * msgin, void * capsule_in)
 {
   const std_msgs__msg__Float64 * msg = (const std_msgs__msg__Float64 *)msgin;
+
+  sim_solver_capsule *capsule = (sim_solver_capsule *) capsule_in;
+  sim_config *acados_sim_config = pendulum_ode_acados_get_sim_config(capsule);
+  sim_in *acados_sim_in = pendulum_ode_acados_get_sim_in(capsule);
+  sim_out *acados_sim_out = pendulum_ode_acados_get_sim_out(capsule);
+  void *acados_sim_dims = pendulum_ode_acados_get_sim_dims(capsule);
   // if (msg == NULL) {
   //   printf("Callback: msg NULL\n");
   // } else {
   //   printf("Callback: I heard: %s\n", msg->data.data);
   // }
   // TODO:
-  pub_msg.data = msg.data;
+
+  sim_in_set(acados_sim_config, acados_sim_dims,
+      acados_sim_in, "x", x_current);
+  status = pendulum_ode_acados_sim_solve(capsule);
+
+  if (status != ACADOS_SUCCESS)
+  {
+      printf("acados_solve() failed with status %d.\n", status);
+  }
+
+  sim_out_get(acados_sim_config, acados_sim_dims,
+          acados_sim_out, "x", x_current);
+
+  printf("\nx_current, %d\n", ii);
+  for (int jj = 0; jj < NX; jj++)
+  {
+      printf("%e\n", x_current[jj]);
+  }
+
+  pub_msg.data = x_current[1];
   rc = rcl_publish(&my_pub, &pub_msg, NULL);
 }
 
 /******************** MAIN PROGRAM ****************************************/
 int main(int argc, const char * argv[])
 {
+
+  int status = 0;
+
+  sim_solver_capsule *capsule = pendulum_ode_acados_sim_solver_create_capsule();
+  status = pendulum_ode_acados_sim_create(capsule);
+
+  if (status)
+  {
+      printf("acados_create() returned status %d. Exiting.\n", status);
+      exit(1);
+  }
+
+  sim_config *acados_sim_config = pendulum_ode_acados_get_sim_config(capsule);
+  sim_in *acados_sim_in = pendulum_ode_acados_get_sim_in(capsule);
+  sim_out *acados_sim_out = pendulum_ode_acados_get_sim_out(capsule);
+  void *acados_sim_dims = pendulum_ode_acados_get_sim_dims(capsule);
+
+  // initial condition
+  x_current[0] = 0;
+  x_current[1] = 3.141592653589793;
+  x_current[2] = 0;
+  x_current[3] = 0;
+
+  sim_in_set(acados_sim_config, acados_sim_dims,
+      acados_sim_in, "x", x_current);
   rcl_allocator_t allocator = rcl_get_default_allocator();
   rclc_support_t support;
   rcl_ret_t rc;
@@ -102,14 +168,14 @@ int main(int argc, const char * argv[])
   ////////////////////////////////////////////////////////////////////////////
   rclc_executor_t executor;
   executor = rclc_executor_get_zero_initialized_executor();
-  // total number of handles = #subscriptions 
+  // total number of handles = #subscriptions
   unsigned int num_handles = 1 + 1;
   printf("Debug: number of DDS handles: %u\n", num_handles);
   rclc_executor_init(&executor, &support.context, num_handles, &allocator);
 
   // add subscription to executor
   rc = rclc_executor_add_subscription_with_context(
-    &executor, &my_sub, &sub_msg, &my_subscriber_callback, capsule,
+    &executor, &my_sub, &sub_msg, &my_subscriber_callback, (void *) capsule,
     ON_NEW_DATA);
   if (rc != RCL_RET_OK) {
     printf("Error in rclc_executor_add_subscription. \n");
@@ -129,6 +195,14 @@ int main(int argc, const char * argv[])
 
   std_msgs__msg__Float64__fini(&pub_msg);
   std_msgs__msg__Float64__fini(&sub_msg);
+
+  // free solver
+  status = pendulum_ode_acados_sim_free(capsule);
+  if (status) {
+      printf("pendulum_ode_acados_sim_free() returned status %d. \n", status);
+  }
+  pendulum_ode_acados_sim_solver_free_capsule(capsule);
+
 
   if (rc != RCL_RET_OK) {
     printf("Error while cleaning up!\n");
